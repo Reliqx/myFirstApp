1c7f284e18803c6206a0d1412b4da00ac1a01f12
Reliqx <32685242+Reliqx@users.noreply.github.com>
2018-03-04 22:08:20 -0500

    registration+login
    login stuff

--- mysite/personalSite/views.py
+++ mysite/personalSite/views.py
@@ -1,48 +1,56 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.views.generic import View
-from .forms import UserForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from personalSite.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
+
+
+
+def user(request):
+    return render(request, 'personal/user.html')
+
+def logout_view(request):
+    logout(request)
+    args = {'logoutmsg': 'sucessful logout!'}
+    return render(request, 'personal/home.html',args)
 def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
     return render(request, 'personal/basic.html' ,{'content': ['if you would like to contact me, please email me', 'relq09@gmail.com']})
 
-class UserFormView(View):
-    form_class = UserForm
-    template_name = 'personal/registration_form.html'
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return redirect('/register')
+    else:
+        form = RegistrationForm()
 
-    #display blank form
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form} )
-    #process form data
-    def post(self, request):
-        form = self.form_class(request.POST)
+        args = {'form': form}
+        return render(request, 'personal/registration_form.html', args)
+
+def profile(request):
+    args = {'user':request.user}
+    return render(request,'personal/profile.html',args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
-            #creates an object from the form, doesnt save to the database
-            user = form.save(commit=False)
-
-            #clean (normalized data)
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-
-            #to change pw
-            user.set_password(password)
-            user.save()
-
-            #return user objects if credentials are correct
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('personal/home.html')
-        return render(request, self.template_name, {'form': form} )
-
-                
+            form.save()
+            return redirect('/user/profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'personal/edit_profile.html',args)
+        
+        
                 
 
                 
